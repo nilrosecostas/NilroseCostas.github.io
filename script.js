27eab9715df025c07b4f79142f007c16e251b41a
nilrosecostas <143980871+nilrosecostas@users.noreply.github.com>
2023-09-04 17:40:37 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const header = document.querySelector('header h1');
-header.addEventListener('mouseover', function() {
-    this.style.color = 'blue';
-});
-
-header.addEventListener('mouseout', function() {
-    this.style.color = '#333';
-});
